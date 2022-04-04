cd00f346b5d6206b4e69f8ef85103a7350738220
Paul Crocker <crocker@di.ubi.pt>
2022-04-04 20:45:08 +0000

    env.js

---
+++ env.js
@@ -0,0 +1,21 @@
+const PPPROD = {
+    'protocol':'https',
+    'host':'glass.epos.ubi.pt:8080',
+    'gf':'/GlassFramework/webresources'
+}
+const PPDEVext = {
+    'protocol':'https',
+    'host':'glass.epos.ubi.pt:8083',
+    'gf':'/GlassFramework/webresources'
+}
+const PPDEVint = {
+    'protocol':'http',
+    'host':'10.0.7.65:8083',
+    'gf':'/GlassFramework/webresources'
+}
+const localhost = {
+    'protocol':'http',
+    'host':'glass.epos.ubi.pt:8080',
+    'gf':'/GlassFramework/webresources'
+}
+const env={ 'PPPROD':PPPROD,'PPPRODext':PPPRODext,'PPPRODint':PPPRODint,'localhost': localhost };
